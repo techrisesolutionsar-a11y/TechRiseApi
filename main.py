d7326c7af452820c60757034568598402578bf41
unknown <jesusrodrigo881@gmail.com>
2025-11-03 22:09:23 -0600

    changes emails api

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from fastapi.middleware.cors import CORSMiddleware
 import smtplib, ssl
 import os
 from dotenv import load_dotenv
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 app = FastAPI()
 
@@ -23,15 +25,64 @@ app.add_middleware(
 def read_root():
     return {"Welcome API": "Welcome to API TECH RISE SOLUTIONS"}
 
-@app.post("/email-contact")
-def emailContact():
+@app.post("/email-contact", tags=["form"])
+def emailContact(form: dict):
     load_dotenv()
+    print(form["name"])
     port = 465
-    password = input("introduce la contraseña: ")
-    sender_email = "techrisesolutionsar@gmail.com"
+    sender_email = os.getenv("EMAIL_SENDER")
+    recive_email = form["email"]
     context = ssl.create_default_context()
-    message = "entrada de datos que ingrese el usuario"
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "TechRiseSolutions"
+    message["From"] = sender_email
+    message["To"] = recive_email
+    text = """\
+        Gracias por tú respuesta en la página de TechRiseSolution, nos pondremos en contacto contigo a la brevedad.
+    """\
+    
+    html = """\
+    <html>
+        <body>
+            <p>Gracias por tú mensaje, nos pondremos en contacto contigo a la brevedad.</p>
+            <a href="https://techrisesolution.netlify.app">TechRiseSolutions</a>
+            <br/><img alt="Tarjeta de presentación de TechRiseSolutions" width="800" height="400" src="https://ibb.co/fg3pbJQ"/>
+        </body>
+    </html>
+    """\
+    
+    part1 = MIMEText(text, "plain")
+    part2 = MIMEText(html, "html")
+    message.attach(part1)
+    message.attach(part2)
+    messageForUs = MIMEMultipart("alternative")
+    messageForUs["Subject"] = "TechRiseSolutions"
+    messageForUs["From"] = sender_email
+    messageForUs["To"] = sender_email
+    htmlForUs = f"""\
+    <html>
+        <body>
+            <h3>Nuevo Cliente:</h3>
+            <p>Cliente: {form["name"]}</p>
+            <p>Teléfono: {form["phone"]}</p>
+            <p>Correo: {form["email"]}</p>
+            <p>Servicio: {form["service"]}</p>
+            <p>Detalles: {form["details"]}</p>
+        </body>
+    </html>
+    """\
+    
+    messageForUsPart1 = MIMEText(htmlForUs, "html")
+    messageForUs.attach(messageForUsPart1)
+    
+    
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login("techrisesolutionsar@gmail.com", os.getenv("PASWWORD_EMAIL"))
-        server.sendmail(sender_email, sender_email, message)
+        server.login(sender_email, os.getenv("PASWWORD_EMAIL"))
+        try:
+            server.sendmail(sender_email, recive_email, message.as_string())
+            server.sendmail(sender_email, sender_email, messageForUs.as_string())
+            return {"status": True, "message": "Correo envíado con éxito."}
+        except Exception as e:
+            print(e)
+            return {"status": False, "message": "No se pudo envíar el correo, intenta más tarde."}
     return {"email": "send email"}
